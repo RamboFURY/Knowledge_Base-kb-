5fcfcc71060f1a64d7a96f5938f41b8dbecc3e09
Saurabh Yadav <rajudubey@protonmail.com>
2019-05-30 13:37:37 +0530

    Correction

--- login.php
+++ login.php
@@ -12,42 +12,66 @@
           align: center;
         }
         </style>
+        <script src="vendor/jquery-validation/dist/jquery.validate.min.js"></script>
+        <script src="js/form-validation.js"></script>
 
     </head>
     <body>
       <div class="loginForm">
         <h1>Login</h1>
-        <form action="validate.php" method="post">
+        <form action="validate.php" method="post" name="login">
         <p>
           <label for="username">Username: </label>
           <input type="text" name="username" placeholder="Enter Username" id="username">
-        </p>
-        <p>
+                                                                                                                                <p>
           <label for="password">Password: </label>
           <input type="password" name="password" placeholder="Enter Password" id="password"><br>
         </p>
-        <input type="submit" value="Login">
-      <div>
-</form>
-<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
-   $(document).ready(function() {
-      $("#form").validate();
-   });
-   jQuery(document).ready(function() {
-   jQuery("#forms).validate({
+        <p>
+          <input type="submit" value="Login">
+        </p>
+      </div>
+      </form>
+
+  <script>
+        $(function() {
+          // Initialize form validation on the registration form.
+          // It has the name attribute "registration"
+          $("form[name='login']").validate({
+          // Specify validation rules
       rules: {
-         username: {
-            required: true,
-            maxlength: 20,
-             },
+        username: {
+          required: true,
+          minlength: 5,
+          maxlength: 20
+        },
         password: {
           required: true,
           minlength: 5,
-          maxlength: 20,
+          maxlenght: 20
         }
+      },
+      // Specify validation error messages
+      messages: {
+        username: {
+          required: "Please provide a username",
+          minlength: "Your username must be at least 5 characters long",
+          maxlength: "Your username must be less than 20 characters long"
+        },
+        password: {
+          required: "Please provide a password",
+          minlength: "Your password must be at least 5 characters long",
+          maxlength: "Your password must be less than 20 characters long"
+        },
+      },
+      // Make sure the form is submitted to the destination defined
+      // in the "action" attribute of the form when valid
+      submitHandler: function(form) {
+        form.submit();
       }
     });
-});
-</script>
+  });
+  </script>
+
     </body>
 </html>
