8dd8a15f0c715cd1896894d78ad9a264bedd21ec
Saurabh Yadav <rajudubey@protonmail.com>
2019-05-31 12:57:32 +0530

    Fixed form layout

--- register.php
+++ register.php
@@ -2,36 +2,58 @@
     <head>
         <title>Register - Knowledge Center</title>
         <style>
-        div.registerForm {
+        div.registrationForm {
           margin: auto;
           width: 20%;
           padding: 10%;
-          border: 2px dotted blue
         }
         h1 {
           align: center;
+        }
+        table, th, td {
+          border: 1px solid black;
+          border-collapse: collapse;
+        }
+        td.button {
+          text-align: center;
         }
         </style>
 
     </head>
     <body>
+      <?php
+      if(isset($_POST['submit']))
+      {
+
+      }
+      ?>
       <div class="registrationForm">
-        <h1>Register</h1>
-        <form action="validate.php" method="post">
-        <p>
-          <label for="username">Username: </label>
-          <input type="text" name="username" placeholder="Enter Username" id="username">
-        </p>
-        <p>
-          <label for="email_id">Email_Id:</label>
-          <input type="text" name="Email_Id" placeholder="Enter Email_Id" id="Email_Id">
-        </p>
-        <p>
-          <label for="password">Password: </label>
-          <input type="password" name="password" placeholder="Enter Password" id="password"><br>
-        </p>
-        <input type="submit" value="Login">
-      <div>
-</form>
-    </body>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+          <table>
+            <tr>
+              <th  colspan="2">Register</th>
+            </tr>
+            <tr>
+              <td><label for="name">Name: </label></td>
+              <td><input type="text" name="name" placeholder="Enter Name" id="name"></td>
+            </tr>
+            <tr>
+            <td><label for="username">Enter Username: </label></td>
+            <td><input type="text" name="username" placeholder="Enter Username" id="username"></td>
+          </tr>
+          <tr>
+            <td><label for="email">Email address:</label></td>
+            <td><input type="email" name="email" placeholder="Enter Email Address" id="email"></td>
+          </tr>
+          <tr>
+            <td><label for="password">Password: </label></td>
+            <td><input type="password" name="password" placeholder="Enter Password" id="password"><br></td>
+          </tr>
+          <tr>
+            <td class="button"  colspan="2"><input type="submit" value="Register" style:></td>
+          </tr>
+        </table>
+      </form>
+    </div>
+  </body>
 </html>
