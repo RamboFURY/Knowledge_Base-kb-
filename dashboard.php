2b6b201d7e95c84d574042561da1d520d0719242
Ashutosh Mishra <ashumishra01999@gmail.com>
2019-05-31 13:41:00 +0530

    created dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,17 +1,43 @@
-<?php
-session_start();
+<html>
+<head>
+<style>
 
-if(isset($_SESSION['username']))
-{
-     echo "<h3>Logged in as user - {$_SESSION['username']}</h3>";
-?>
-     <hr>
-     <a href="logout.php">Logout</a>
-<?php
-}
-else
-{
-    echo "<h3>Unauthorised Access<h3>";
-}
 
-?>
+.centernav {
+  overflow: hidden;
+  font-size: 20%;
+  text-align: center;
+  margin-top: 15%;
+  padding: 6px;
+}
+.centernav input[type=text] {
+
+  font-size: 50px;
+  background-color: rgba(0, 0, 0, 0.1);
+  border: 2px solid #FFFF
+
+}
+.addnew{
+  overflow: hidden;
+  font-size: 20%;
+  text-align: left;
+  position: absolute;
+
+}
+</style>
+</head>
+
+<body>
+
+<div class="centernav">
+    <form action="validate.php" method="post" name="centernav">
+  <input type="text"  placeholder="Search..">
+<div class="addnew">
+    <form action="validate.php" method="post" name="Add issue">
+  <p>
+    <input type="submit" value="Add issue">
+  </p>
+</div>
+  </div>
+</body>
+</html>
