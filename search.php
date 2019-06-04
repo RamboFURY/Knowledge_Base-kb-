0a5cebd3469c51e19118c2db54abb8501f0b9f61
unknown <yf.yousuf95@gmail.com>
2019-06-04 13:50:19 +0530

    search.php

--- search.php
+++ search.php
@@ -11,4 +11,27 @@ else
 {
   die("<h3>Unauthorised Access<h3>");
 }
+require_once('dbconnect.php');
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Search Results</title>
+  </head>
+  <body>
+    <?php
+    $dbconnection = new dbconnector;
+    $dbconnection->connect();
+    $rows = $dbconnection->search($_GET['query']);
+    echo '<table>';
+    if(count($rows) > 0){
+      echo '<tr>';
+      echo "<td>$rows[0]->['title']";
+      echo '</td>';
+      echo '</tr>';
+    }
+
+     ?>
+  </body>
+</html>
