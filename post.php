903b4b18414ff8a321c012c353281cf57a0f72e3
Ashutosh Mishra <ashumishra01999@gmail.com>
2019-06-10 13:58:00 +0530

    create post.php page

---
+++ post.php
@@ -0,0 +1,79 @@
+<!Doctype html>
+<html>
+<head>
+  <style>
+  .wrapper {
+    display: flex;
+  }
+
+  .wrapper>section {
+    font-size: 4vh;
+    color: black;
+    margin: .1em;
+    padding: .3em;
+    border-radius: 3px;
+    flex: 1;
+  }
+div.postform{
+  display: block;
+  margin: 2% auto;
+  margin-left:10%;
+  text-align:center;
+  border: 2px solid #3498db;
+  padding: 14px 10px;
+  width: 50%;
+  outline: none;
+  color: black;
+  border-radius: 20px;
+  }
+  div.navbar{
+    display: block;
+    margin: 2% auto;
+    text-align:center;
+    border: 2px solid #3498db;
+    padding: 14px 10px;
+    width: 50%;
+    outline: none;
+    color: black;
+    border-radius: 20px;
+  }
+  div.backbutton{
+
+
+}
+</style>
+</head>
+<body>
+  <section class="wrapper">
+  <div class="navbar">
+    <nav>
+
+    </nav>
+  </section>
+  <hr>
+  <section class="wrapper">
+    <div class="postform">
+    <p class="title">title</p>
+    <p class="description">discription</p>
+    <p class="resolution">resolution</p>
+  </div>
+  <div class="backbutton">
+  <form class="backbutton" action="back.php">
+  <input type="submit"  class="back" value="back">
+</div>
+  </section>
+
+
+
+
+
+
+
+
+</body>
+<footer>
+
+
+
+</footer>
+</html>
