46a854d87818d60e963b05cda56803a5401f6ad9
Saurabh Yadav <rajudubey@protonmail.com>
2019-06-18 11:50:09 +0530

    fix for validate

--- validate.php
+++ validate.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include("dbconnect.php");
 $dbconnection = new dbconnector;
 $dbconnection->connect();
@@ -7,7 +8,6 @@ $result = $dbconnection->checkLogin($_POST['username'], $_POST['password']);
 $row = $result->fetch_array(MYSQLI_ASSOC);
 if($result->num_rows > 0)
 {
-  session_start();
   $_SESSION['username'] = $_POST['username'];
   $_SESSION['user_id'] = $row['id'];
   $_SESSION['name'] = $row['name'];
