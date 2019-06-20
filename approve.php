164bc779d0f7d7fe1a2237def85ec8dd1b45d1a9
sahil pathania <sahil.pathania.3979@gmail.com>
2019-06-20 12:29:22 +0530

    added comments in approve.php

--- approve.php
+++ approve.php
@@ -1,11 +1,11 @@
 <?php
-require_once("dbconnect.php");
+require_once("dbconnect.php");//requires once to establish connection with DB
 session_start();
 if(isset($_POST['auth_id']))
 {
   $dbconnection = new dbconnector;
   $dbconnection->connect();
-  $status = $dbconnection->approvepost($_POST['auth_id'], 1);
+  $status = $dbconnection->approvepost($_POST['auth_id'], 1);//approval of new issue
   if($status == 1)
   {
     $_SESSION['approved'] = true;
